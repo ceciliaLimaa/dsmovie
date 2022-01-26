ff47c5e80ae4862dc79c8b570f4d80a647774be5
ceciliaLimaa <anacecilialima.ale@gmail.com>
2022-01-26 19:04:34 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,40 @@
+import MovieCard from "components/MovieCard";
 import Pagination from "components/pagination";
 
-function Listing(){
-    return(
-     <Pagination />
+function Listing() {
+    return (
+        <>
+            <Pagination />
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3"> {/*div de cada filme para voto, os tamanhos dos cards de acordo com a largura da tela*/}
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+        </>
     );
 }
 export default Listing;
+
+/* A função do componente react so pode exportar 1 componente  
+        sendo necessario colocar em div
+        porém apareceria um div a mais no projeto. 
+        o ideal é colocar somente o fragment(<></>) pois n enterfere no projeto*/
