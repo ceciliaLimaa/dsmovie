06331b74832399bf66299b7aa8efe316d6c09555
ceciliaLimaa <anacecilialima.ale@gmail.com>
2022-02-07 21:14:44 -0300

    Link e movie (form)

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,5 +1,6 @@
-import { Link, useParams } from 'react-router-dom';
-import { Movie } from 'types/movie';
+import {  useParams } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+// import { Movie } from 'types/movie';
 import FormCard from 'components/FormCard';
 
 function Form() {
